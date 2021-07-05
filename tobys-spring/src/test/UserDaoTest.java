5a99d6cdc8b6a12a968987150279b8d3c9a3818d
gosuljo <lurutia@naver.com>
2021-07-05 22:28:38 +0900

    [UPDATE] JUnit 테스트 추가

--- tobys-spring/src/test/UserDaoTest.java
+++ tobys-spring/src/test/UserDaoTest.java
@@ -3,13 +3,23 @@ package test;
 import dao.UserDao;
 import dao.module.DaoFactory;
 import model.User;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
+        JUnitCore.main("test.UserDaoTest");
+    }
+
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 //        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
@@ -27,16 +37,10 @@ public class UserDaoTest {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = userDao.get(user.getId());
+        user2.setName("user03");
 
-        if (!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (name)");
-        }
-        else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        }
-        else {
-            System.out.println("조회 테스트 성공");
-        }
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
 //        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
 //        System.out.println("Connection counter : " + ccm.getCounter());
     }
