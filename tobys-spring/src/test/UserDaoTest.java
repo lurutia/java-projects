2c02d59de72fbf7ddb3b6a8573825572c66a3f4c
gosuljo <lurutia@naver.com>
2021-07-05 22:19:32 +0900

    [REFACTOR] 테스트 코드 결과 확인하도록 수정

--- tobys-spring/src/test/UserDaoTest.java
+++ tobys-spring/src/test/UserDaoTest.java
@@ -1,12 +1,10 @@
 package test;
 
 import dao.UserDao;
-import dao.module.CountingConnectionMaker;
 import dao.module.DaoFactory;
 import model.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -29,11 +27,16 @@ public class UserDaoTest {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = userDao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
-
+        if (!user.getName().equals(user2.getName())) {
+            System.out.println("테스트 실패 (name)");
+        }
+        else if (!user.getPassword().equals(user2.getPassword())) {
+            System.out.println("테스트 실패 (password)");
+        }
+        else {
+            System.out.println("조회 테스트 성공");
+        }
 //        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
 //        System.out.println("Connection counter : " + ccm.getCounter());
     }
