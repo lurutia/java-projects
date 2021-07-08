9da4b2b69b772152e4731f45f296e4e2539377dc
gosuljo <lurutia@naver.com>
2021-07-08 22:28:04 +0900

    [UPDATE] addAndGet Test 개선

--- tobys-spring/src/dao/UserDao.java
+++ tobys-spring/src/dao/UserDao.java
@@ -73,4 +73,20 @@ public class UserDao {
 
         return true;
     }
+
+    public int getCount() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+    }
 }
--- tobys-spring/src/test/UserDaoTest.java
+++ tobys-spring/src/test/UserDaoTest.java
@@ -14,9 +14,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class UserDaoTest {
-    public static void main(String[] args) {
-        JUnitCore.main("test.UserDaoTest");
-    }
 
     @Test
     public void addAndGet() throws SQLException, ClassNotFoundException {
@@ -24,24 +21,47 @@ public class UserDaoTest {
 //        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao userDao = context.getBean("userDao", UserDao.class);
+        User user = new User("user01", "유저01", "12345");
+        User user2 = new User("user02", "유저02", "12345");
 
         userDao.delete();
-
-        User user = new User();
-        user.setId("2");
-        user.setName("user02");
-        user.setPassword("12345");
+        assertThat(userDao.getCount(), is(0));
 
         userDao.add(user);
+        userDao.add(user2);
+        assertThat(userDao.getCount(), is(2));
 
-        System.out.println(user.getId() + " 등록 성공");
+        User getUser1 = userDao.get(user.getId());
+        assertThat(getUser1.getName(), is(user.getName()));
+        assertThat(getUser1.getPassword(), is(user.getPassword()));
 
-        User user2 = userDao.get(user.getId());
-        user2.setName("user03");
+        User getUser2 = userDao.get(user2.getId());
+        assertThat(user2.getName(), is(user2.getName()));
+        assertThat(user2.getPassword(), is(user2.getPassword()));
 
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
 //        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
 //        System.out.println("Connection counter : " + ccm.getCounter());
+    }
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("user1", "유저1", "12345");
+        User user2 = new User("user2", "유저2", "12345");
+        User user3 = new User("user3", "유저3", "12345");
+
+        userDao.delete();
+        assertThat(userDao.getCount(), is(0));
+
+        userDao.add(user1);
+        assertThat(userDao.getCount(), is(1));
+
+        userDao.add(user2);
+        assertThat(userDao.getCount(), is(2));
+
+        userDao.add(user3);
+        assertThat(userDao.getCount(), is(3));
     }
 }
