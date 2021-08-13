5bd4ce3c8cc3d01cc286423c5357bd181be83026
gosuljo <lurutia@naver.com>
2021-08-14 03:35:43 +0900

    [REFACTOR] JdbcContext class로 분리

--- tobys-spring/src/dao/UserDao.java
+++ tobys-spring/src/dao/UserDao.java
@@ -9,6 +9,7 @@ import java.sql.*;
 public class UserDao {
 //    private ConnectionMaker dataSource;
     private DataSource dataSource;
+    private JdbcContext jdbcContext;
 
     public UserDao() {
 
@@ -16,6 +17,10 @@ public class UserDao {
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void setJdbcContext(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
     }
 
     //    public void setConnectionMaker(ConnectionMaker dataSource) {
@@ -26,7 +31,7 @@ public class UserDao {
 //    }
 
     public void add(User user) throws SQLException {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
@@ -67,7 +72,7 @@ public class UserDao {
 
     public boolean delete() throws SQLException {
         StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        this.jdbcContext.workWithStatementStrategy(st);
         return true;
     }
 
@@ -85,21 +90,5 @@ public class UserDao {
         c.close();
 
         return count;
-    }
-
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = stmt.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch(SQLException e) {
-            throw e;
-        } finally {
-            if(ps != null) { try { ps.close(); } catch (SQLException e) { } }
-            if(c != null) { try { c.close(); } catch (SQLException e) { } }
-        }
     }
 }
