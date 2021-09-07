1adbf7b29a85c5dd667da50afc53dfb9be6437e8
gosuljo <lurutia@naver.com>
2021-09-07 21:29:10 +0900

    [UPDATE] UserService Test 개선

--- tobys-spring/src/model/Level.java
+++ tobys-spring/src/model/Level.java
@@ -1,16 +1,22 @@
 package model;
 
 public enum Level {
-    BASIC(1), SILVER(2), GOLD(3);
+    GOLD(3, null), SILVER(2, GOLD), BASIC(1, SILVER) ;
 
     private final int value;
+    private final Level next;
 
-    Level(int value) {
+    Level(int value, Level next) {
         this.value = value;
+        this.next = next;
     }
 
     public int intValue() {
         return value;
+    }
+
+    public Level nextLevel() {
+        return this.next;
     }
 
     public static Level valueOf(int value) {
@@ -20,5 +26,13 @@ public enum Level {
             case 3: return GOLD;
             default: throw new AssertionError("Unknown value: " + value);
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Level{" +
+                "value=" + value +
+                ", next=" + next +
+                '}';
     }
 }
--- tobys-spring/src/test/UserServiceTest.java
+++ tobys-spring/src/test/UserServiceTest.java
@@ -15,28 +15,29 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
+import static user.service.UserService.MIN_LOGOUT_FOR_SILVER;
+import static user.service.UserService.MIN_RECCOMEND_FOR_GOLD;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/test-applicationContext.xml"})
+@ContextConfiguration(locations = {"classpath*:/test-applicationContext.xml"})
 public class UserServiceTest {
     @Autowired
-    UserService userService;
+    UserDao userDao;
 
     @Autowired
-    UserDao userDao;
+    UserService userService;
 
     List<User> users;
 
     @Before
     public void setUp() {
         users = Arrays.asList(
-                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
-                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
-                new User("erwins", "신승한", "p3", Level.SILVER, 60, 29),
-                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
-                new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+                new User("bumjin", "박범진", "p1", Level.BASIC, MIN_LOGOUT_FOR_SILVER-1, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, MIN_LOGOUT_FOR_SILVER, 0),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD-1),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD),
+                new User("green", "오민규", "p5", Level.GOLD, 100, Integer.MAX_VALUE)
         );
     }
 
@@ -49,11 +50,17 @@ public class UserServiceTest {
 
         userService.upgradeLevel();
 
-        checkLevel(users.get(0), Level.BASIC);
-        checkLevel(users.get(1), Level.SILVER);
-        checkLevel(users.get(2), Level.SILVER);
-        checkLevel(users.get(3), Level.GOLD);
-        checkLevel(users.get(4), Level.GOLD);
+        checkLevelUpgraded(users.get(0), false);
+        checkLevelUpgraded(users.get(1), true);
+        checkLevelUpgraded(users.get(2), false);
+        checkLevelUpgraded(users.get(3), true);
+        checkLevelUpgraded(users.get(4), false);
+
+//        checkLevel(users.get(0), Level.BASIC);
+//        checkLevel(users.get(1), Level.SILVER);
+//        checkLevel(users.get(2), Level.SILVER);
+//        checkLevel(users.get(3), Level.GOLD);
+//        checkLevel(users.get(4), Level.GOLD);
     }
 
     @Test
@@ -77,5 +84,15 @@ public class UserServiceTest {
     private void checkLevel(User user, Level expectedLevel) {
         User userUpdate = userDao.get(user.getId());
         assertThat(userUpdate.getLevel(), is(expectedLevel));
+    }
+
+    private void checkLevelUpgraded(User user, boolean upgraded) {
+        User userUpdate = userDao.get(user.getId());
+        if (upgraded) {
+            assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
+        }
+        else {
+            assertThat(userUpdate.getLevel(), is(user.getLevel()));
+        }
     }
 }
